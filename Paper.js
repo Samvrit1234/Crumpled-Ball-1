084856d238bb88b32762323374ac7498e7e8329a
Samvrit1234 <68464137+Samvrit1234@users.noreply.github.com>
2020-08-25 18:37:13 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,20 @@
+class Paper{
+    constructor(x, y, width, height){
+   var options = {
+       isStatic : false,
+       restitution: 0.3,
+       density : 1.2
+   }
+
+       this.body = Bodies.circle(x, y, width, height, options);
+       this.width = width;
+       this.height = height;
+
+       World.add
+
+  
+    }
+    display(){
+        FileList("pink")
+    }
+} 
